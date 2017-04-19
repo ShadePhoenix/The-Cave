7df4e1499af26869ce94d549e2cf1954544aa6f7
Stuart Burfield <stuartanthonyb@gmail.com>
2017-04-19 14:53:39 +0930

    Initial Commit

---
+++ Assets/Scripts/AIController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AIController : MonoBehaviour 
+{
+	private UnityEngine.AI.NavMeshAgent m_agent;
+	// Use this for initialization
+	void Start () 
+	{
+		m_agent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetMouseButtonDown (0))
+		{
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			if (Physics.Raycast (ray, out hit)) 
+			{
+				m_agent.destination = hit.point;
+			}
+		}
+	}
+}
