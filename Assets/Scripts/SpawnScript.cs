1c1c4752e9cabcff7d3d3f6989765f5051bdb81f
AIE\Daniel <shadow_cloud_master@hotmail.com>
2017-04-20 09:20:38 +0930

    Fixed Line Ending on SpawnScript.

--- Assets/Scripts/SpawnScript.cs
+++ Assets/Scripts/SpawnScript.cs
@@ -2,56 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnScript : MonoBehaviour {
+public class SpawnScript : MonoBehaviour
+{
 
     [Tooltip("Add the enemy to be spawned.")]
     public Transform enemy;
     [Tooltip("The time (in seconds) between enemies spawning.")]
     public float enemySpawnInterval = 3;
-	[Tooltip("The maximum number of enemies that can spawn on this level.")]
-	public int maxEnemiesToSpawn;
-	[Tooltip("The minimum number of enemies that can spawn on this level.")]
-	public int minEnemiesToSpawn;
-    
-	private float timeUntilSpawn = 0;
+    [Tooltip("The maximum number of enemies that can spawn on this level.")]
+    public int maxEnemiesToSpawn;
+    [Tooltip("The minimum number of enemies that can spawn on this level.")]
+    public int minEnemiesToSpawn;
+
+    private float timeUntilSpawn = 0;
     private GameObject[] spawners;
     //private bool addedSpawners = false;
-	private bool spawning = false;
-	
+    private bool spawning = false;
+
 
     // Use this for initialization
     void Start()
     {
-       // GameObject[] tempSpawners;
+        // GameObject[] tempSpawners;
         spawners = GameObject.FindGameObjectsWithTag("Spawners");
     }
 
     // Update is called once per frame
     void Update()
     {
-		if (spawning == false)
-		{
-			timeUntilSpawn -= Time.deltaTime;
-			
-			// make coroutine
-			if (timeUntilSpawn <= 0)
-			{
-				spawning = true;		
-				Instantiate(enemy, transform.position, Quaternion.identity);				
-				// StartCoroutine("SpawnEnemy");
-				timeUntilSpawn = enemySpawnInterval;
-			}
-		}       
+        if (spawning == false)
+        {
+            timeUntilSpawn -= Time.deltaTime;
+
+            // make coroutine
+            if (timeUntilSpawn <= 0)
+            {
+                spawning = true;
+                Instantiate(enemy, transform.position, Quaternion.identity);
+                // StartCoroutine("SpawnEnemy");
+                timeUntilSpawn = enemySpawnInterval;
+            }
+        }
     }
 
-   // private IEnumerator SpawnEnemy()
+    // private IEnumerator SpawnEnemy()
     // {
-		// private int randSpawn = Random.Range(0, spawners.length); // pick a random spawner index in the array
-		// private int enemiesToSpawn = Random.Range(minEnemiesToSpawn, maxEnemiesToSpawn);
-        // while (enemiesToSpawn > 0)
-        // {
-			// enemiesToSpawn--;
-			// Instantiate(enemy, spawners[randSpawn].transform.position, Quaternion.identity);
-            // yield return new WaitForSeconds(0.5);            
-        // }
+    // private int randSpawn = Random.Range(0, spawners.length); // pick a random spawner index in the array
+    // private int enemiesToSpawn = Random.Range(minEnemiesToSpawn, maxEnemiesToSpawn);
+    // while (enemiesToSpawn > 0)
+    // {
+    // enemiesToSpawn--;
+    // Instantiate(enemy, spawners[randSpawn].transform.position, Quaternion.identity);
+    // yield return new WaitForSeconds(0.5);            
     // }
+    // }
+}
