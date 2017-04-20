6ab114b43c23be8384d71a2d7abb9aa9250901bf
Stuart B <stuartanthonyb@gmail.com>
2017-04-21 08:17:29 +0930

    public variables for spawning. About to make spawning happen more frequently each night.

--- Assets/Scripts/SpawnEnemy.cs
+++ Assets/Scripts/SpawnEnemy.cs
@@ -7,9 +7,16 @@ public class SpawnEnemy : MonoBehaviour
     [Tooltip("Add the enemy to be spawned.")]
     public GameObject enemy;
     [Tooltip("The time (in seconds) between enemies spawning.")]
-    public float enemySpawnInterval = 3;   
-    private int maxEnemiesToSpawn = 3;    
-    private int minEnemiesToSpawn = 1;
+    public float enemySpawnInterval = 3;
+    [Tooltip("Max enemies that can spawn at a time from the start. This increases by 1 every night.")]
+    public int maxEnemiesToSpawn = 3;
+    [Tooltip("Min enemies that can spawn at a time from the start.")]
+    public int minEnemiesToSpawn = 1;
+    [Tooltip("How slowly enemies spawn at the start of the game. Gradually decreses.")]
+    public float startSpawnTimer = 1;
+    [Tooltip("The shortest interval between enemy spawns. Enemies will never spawn more frequently than this.")]
+    public float finalSpawnTimer = 1;
+
     private List<GameObject> enemies = new List<GameObject>(); // pool of enemies
     private int enemiesToSpawn = 1000;
     private bool genEnemies = false;
