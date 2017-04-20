03f2cbd148b63f882edc62babba8fe1e06d1e62d
AIE\Toby <Tobykelty@gmail.com>
2017-04-20 11:06:16 +0930

    DayNight Script, drag onto directional light, rotates along the x axis

---
+++ Assets/Scripts/Designer Scipts/DayNight.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DayNight : MonoBehaviour {
+    public float nightLength;
+    public float dayLength;
+    public float nightNumber;
+    public bool isNight = false;
+    private float dayRotate = 0;
+    private float nightTimer = 0;
+    private float dayAngle = 240;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (isNight)
+        {
+            nightTimer += Time.deltaTime;
+            Debug.Log(nightTimer);
+            if (nightTimer > nightLength)
+            {
+                isNight = false;
+                nightTimer = 0;
+                transform.Rotate(Vector3.left * dayAngle);
+            }
+        }
+        if (!isNight)
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * dayAngle/dayLength);
+            dayRotate += Time.deltaTime * dayAngle / dayLength;
+            if (dayRotate > dayAngle)
+            {
+                dayRotate = 0;
+                nightNumber += 1;
+                isNight = true;
+            }
+        }
+
+	}
+}
