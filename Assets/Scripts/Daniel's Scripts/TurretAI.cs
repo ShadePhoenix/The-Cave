d83ff052746d95ac528dc70034cef2733191fe22
Stuart B <stuartanthonyb@gmail.com>
2017-04-20 23:14:25 +0930

    Added bullet pooling. Need to finalise it. Also added health meter to ballista prefab.

--- Assets/Scripts/Daniel's Scripts/TurretAI.cs
+++ Assets/Scripts/Daniel's Scripts/TurretAI.cs
@@ -17,8 +17,11 @@ public class TurretAI : MonoBehaviour
     public Transform projectileSpawn;
     [Tooltip("")]
     public GameObject projectile;
-
-    //public GameObject bulletPrefab;
+    private int numProjectiles = 2; // number of projectiles to pool
+    
+    private List<GameObject> bulletList = new List<GameObject>(); // pool of normal bullets   
+    private List<Rigidbody> bulletBodies = new List<Rigidbody>(); // pool of bullet bodies
+    private bool bulletGen = false; // have normal bullets been generated
 
     [Tooltip("How much the unit will cost to build")]
     public int conMatCost;
@@ -53,11 +56,13 @@ public class TurretAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GenBullets();  // Generate a pool bullets if they haven't been already
         currentHealth = health;
         RangeMonitor();
         Aim();
         Fire();
         HealthUpdate();
+        
     }
 
     //Calculates which enemy in range has traveled the furthest and targets them
@@ -105,12 +110,26 @@ public class TurretAI : MonoBehaviour
 
     void Fire()
     {
-        if (fire && target != null)
+        if ((fire && target != null) && bulletGen)
         {
-            GameObject lProjectile = Instantiate(projectile, projectileSpawn.position, Quaternion.Euler(projectileSpawn.transform.eulerAngles));
-            lProjectile.GetComponent<Rigidbody>().AddForce(lProjectile.transform.up * projectileSpeed, ForceMode.Impulse);
-            StartCoroutine(FireWait(fireWait));
-            fire = false;
+            for (int i = 0; i < numProjectiles; i++)
+            {
+                if (bulletList[i].gameObject.activeSelf == false)
+                {
+                    bulletList[i].gameObject.SetActive(true); // reactivate the bullet
+                    // set its position
+                    bulletList[i].transform.position = new Vector3(projectileSpawn.position.x, projectileSpawn.position.y, projectileSpawn.position.z); 
+                    bulletList[i].transform.eulerAngles = projectileSpawn.transform.eulerAngles; // set its ange
+                    bulletBodies[i].AddForce(bulletList[i].transform.up * projectileSpeed, ForceMode.Impulse);  // add force on the matching rigidbody                  
+                    StartCoroutine(FireWait(fireWait));
+                    fire = false;                   
+                    return;
+                }
+            }
+            //GameObject lProjectile = Instantiate(projectile, projectileSpawn.position, Quaternion.Euler(projectileSpawn.transform.eulerAngles));
+            //lProjectile.GetComponent<Rigidbody>().AddForce(lProjectile.transform.up * projectileSpeed, ForceMode.Impulse);
+            //StartCoroutine(FireWait(fireWait));
+            //fire = false;
         }
     }
 
@@ -128,5 +147,19 @@ public class TurretAI : MonoBehaviour
             //Destroy Turret and play particle effects
             //GameObject.FindGameObjectWithTag("Canvas").GetComponent<UIManager>().GameOver();
         }
+    }
+
+    void GenBullets()
+    {
+        if(bulletGen == false)
+        {
+            bulletGen = true;
+            for (int i = 0; i < numProjectiles; i++)
+            {
+                bulletList.Add((GameObject)Instantiate(projectile, new Vector3(-10, -10, 0), Quaternion.identity));
+                bulletList[i].gameObject.SetActive(false);
+                bulletBodies.Add(bulletList[i].GetComponent<Rigidbody>());
+            }
+        }        
     }
 }
