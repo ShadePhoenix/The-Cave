09011f23ed1f11a701300bc29119e6858be066c0
AIE\Toby <Tobykelty@gmail.com>
2017-04-21 11:45:52 +0930

    Edits

---
+++ Assets/Scripts/Designer Scipts/LookAtMouse.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtMouse : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 1000))
+        {
+            Debug.Log("Hit");
+            gameObject.transform.LookAt(hit.transform.position);
+        }
+    }
+}
