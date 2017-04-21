dab2b33bc5a342d046a424ac0d5e3ef6b2b21916
AIE\Toby <Tobykelty@gmail.com>
2017-04-21 13:48:40 +0930

    Short script with copy/paste elements for adding audio to an object. Please note the RequireComponents at start

---
+++ Assets/Scripts/Designer Scipts/AudioPlay.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlay : MonoBehaviour {
+    private AudioSource player;
+    public AudioClip[] effects;
+	// Use this for initialization
+	void Start () {
+        player = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        player.clip = effects[(Random.Range(0, effects.Length))];
+        player.Play();
+	}
+}
