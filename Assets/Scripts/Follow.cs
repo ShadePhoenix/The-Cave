d2fee4554f05f3fada745376d1c0c626d6f43579
Stuart B <stuartanthonyb@gmail.com>
2017-05-01 19:04:04 +0930

    Camera fixed and improved

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -5,20 +5,33 @@ using UnityEngine;
 public class Follow : MonoBehaviour {
 
     public GameObject followObject;
-
-    public Vector3 offset;
+    public Vector3 playerOffset;
+    public Vector3 castleOffset;
+    [HideInInspector]
+    public bool followingPlayer = true;
 
     // Use this for initialization
     void Start()
     {
         followObject = GameObject.FindGameObjectWithTag("Player");
-        transform.position = followObject.transform.position + offset;
-        transform.LookAt(followObject.transform.position);
+        transform.position = followObject.transform.position + playerOffset;
+        //transform.LookAt(followObject.transform.position);
+        transform.eulerAngles = new Vector3(40, 0, 0);
         //offset = new Vector3(0, 20, 20);
     }
 	// Update is called once per frame
 	void Update ()
     {
-        transform.position = Vector3.Lerp(transform.position, followObject.transform.position + offset, Time.deltaTime * 5);
+        if(followingPlayer)
+        {
+            transform.position = followObject.transform.position + playerOffset;
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(transform.position, followObject.transform.position + castleOffset, Time.deltaTime * 5);
+        }
+        
     }
+
+    
 }
