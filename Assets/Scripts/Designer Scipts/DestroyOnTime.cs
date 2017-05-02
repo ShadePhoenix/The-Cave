b7f2a1a0f3a22cf5084421e07b37e8c0519f87a0
Wraith\Admin <tobykelty@gmail.com>
2017-05-02 19:00:02 +0930

    Smaller Particles, destroy on time added to gold, default time is 120 seconds , made coins emmisive (already have lots of lights everwhere) though it kills the detail and doesnt look great

---
+++ Assets/Scripts/Designer Scipts/DestroyOnTime.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyOnTime : MonoBehaviour {
+    public float lifeSpan = 120.0f;
+    private float timer ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        if (lifeSpan < timer)
+        {
+            Destroy(gameObject);
+        }
+	}
+}
