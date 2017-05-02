0aaccab3f7ae0d5ecd36145c522e0af45a6d9e3d
Stuart B <stuartanthonyb@gmail.com>
2017-05-02 21:05:31 +0930

    Added gold script to Gold UI

---
+++ Assets/Scripts/Gold.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Gold : MonoBehaviour {
+
+    public GameObject gold;
+    private Text goldText;
+
+    // Use this for initialization
+    void Start () {
+        goldText = gold.GetComponent<Text>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        goldText.text = "" + UIManager.energy;
+    }
+}
