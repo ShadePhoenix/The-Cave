0c4fc0ded3017e5010412b831bda45a18b991405
Stuart B <stuartanthonyb@gmail.com>
2017-05-02 19:02:38 +0930

    Starting to program Game Over.

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameOver : MonoBehaviour {
+
+    private GameObject gc;
+    private AITargets aiTargets;
+
+    // Use this for initialization
+    void Start ()
+    {
+        gc = GameObject.FindObjectOfType<Main>().gameObject;
+        aiTargets = gc.GetComponent<AITargets>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void RestartGame()
+    {
+        aiTargets.Init();
+    }
+}
