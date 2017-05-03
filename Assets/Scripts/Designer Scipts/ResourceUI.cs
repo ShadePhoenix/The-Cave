5b8acacdd7bc057fc4a2b8a9e98fff0e8f2a7a4c
AIE\Daniel <shadow_cloud_master@hotmail.com>
2017-05-03 10:58:54 +0930

    Highscore saving implemented

--- Assets/Scripts/Designer Scipts/ResourceUI.cs
+++ Assets/Scripts/Designer Scipts/ResourceUI.cs
@@ -11,11 +11,20 @@ public class ResourceUI : MonoBehaviour {
     private Text constructionText;
     private Text goldText;
 
+    public Text highScoreText;
+
+
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
+        if (!PlayerPrefs.HasKey("highScore"))
+            InitializeHS();
+        else
+            highScoreText.text = PlayerPrefs.GetInt("highScore").ToString();
         energyText = energy.GetComponent<Text>();
         constructionText = constructionMaterials.GetComponent<Text>();
         goldText = gold.GetComponent<Text>();
+        UpdateHS();
     }
 	
 	// Update is called once per frame
@@ -23,5 +32,20 @@ public class ResourceUI : MonoBehaviour {
         energyText.text = "" + UIManager.energy;
         constructionText.text = "" + UIManager.conMat;
         goldText.text = "" + UIManager.gold;
+    }
+
+    void InitializeHS()
+    {
+        PlayerPrefs.SetInt("highScore", 0);
+        PlayerPrefs.Save();
+    }
+
+    void UpdateHS()
+    {
+        if (UIManager.gold > PlayerPrefs.GetInt("highScore"))
+        {
+            PlayerPrefs.SetInt("highScore", UIManager.gold);
+            PlayerPrefs.Save();
+        }
     }
 }
